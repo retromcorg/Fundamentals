df2b3e78ffa17b65299ced6327f26128426c70ee
Johny Muffin <jetpackingwolf@gmail.com>
2020-05-23 13:47:03 +1000

    Config, Language File, Autosaving And Basic Home Methods

--- src/com/johnymuffin/beta/fundamentals/CommandUtils.java
+++ src/com/johnymuffin/beta/fundamentals/CommandUtils.java
@@ -4,16 +4,21 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class CommandUtils {
 
 
     public static Player getPlayerFromString(String name) {
         List<Player> players = Bukkit.matchPlayer(name);
-        if(players.size() == 1) {
+        if (players.size() == 1) {
             return players.get(0);
         }
         return null;
+    }
+
+    public static boolean verifyHomeName(String name) {
+        return Pattern.matches("[a-zA-Z]+", name);
     }
 
 
