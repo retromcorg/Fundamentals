5913ebd7aac8bb8e87bffd8fba63f0f8109adb45
Johny Muffin <jetpackingwolf@gmail.com>
2020-05-27 16:17:05 +1000

    Improve project structure for multiple plugins

---
+++ FundamentalsBridge/src/com/johnymuffin/fundamentals/essbridge/FundamentalsESSBridge.java
@@ -0,0 +1,15 @@
+package com.johnymuffin.fundamentals.essbridge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FundamentalsESSBridge extends JavaPlugin {
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
