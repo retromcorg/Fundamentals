39d4e63df2e478202f8dd8d999b8d3102ac88aa8
Johny Muffin <jetpackingwolf@gmail.com>
2020-05-27 16:07:38 +1000

    Permission fix

--- src/com/johnymuffin/beta/fundamentals/FundamentalPermission.java
+++ src/com/johnymuffin/beta/fundamentals/FundamentalPermission.java
@@ -1,15 +1,10 @@
 package com.johnymuffin.beta.fundamentals;
 
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class FundamentalPermission {
 
     public static boolean isPlayerAuthorized(final CommandSender commandSender, final String permission) {
-        if (commandSender instanceof OfflinePlayer) {
-            return false;
-        }
         if (commandSender.isOp()) {
             return true;
         }
