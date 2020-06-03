2aeda39b4826907f564cf21730fd15ab4882d6df
Johny Muffin <jetpackingwolf@gmail.com>
2020-06-03 15:27:15 +1000

    Start Teleport and Importer work

---
+++ FundamentalsCore/src/com/johnymuffin/beta/fundamentals/commands/CommandTeleport.java
@@ -0,0 +1,104 @@
+//package com.johnymuffin.beta.fundamentals.commands;
+//
+//import com.johnymuffin.beta.fundamentals.FundamentalsPlayerMap;
+//import com.johnymuffin.beta.fundamentals.settings.FundamentalsLanguage;
+//import org.bukkit.command.Command;
+//import org.bukkit.command.CommandExecutor;
+//import org.bukkit.command.CommandSender;
+//import org.bukkit.entity.Player;
+//
+//import static com.johnymuffin.beta.fundamentals.CommandUtils.getPlayerFromString;
+//import static com.johnymuffin.beta.fundamentals.FundamentalPermission.isPlayerAuthorized;
+//
+//public class CommandTeleport implements CommandExecutor {
+//    @Override
+//    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+//        if (!isPlayerAuthorized(commandSender, "fundamentals.teleport")) {
+//            commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("no_permission"));
+//            return true;
+//        }
+//
+//
+//        switch (strings.length) {
+//            case 0:
+//                //Not Enough Args
+//            case 1: //Teleporting to a player
+//                Player teleportTo = getPlayerFromString(strings[0]);
+//                if (teleportTo == null) {
+//                    String message = FundamentalsLanguage.getInstance().getMessage("player_not_found_full");
+//                    message = message.replace("%username%", strings[0]);
+//                    commandSender.sendMessage(message);
+//                    return true;
+//                }
+//                //Teleport Player
+//            case 2: //Teleport one player to another
+//                if (!isPlayerAuthorized(commandSender, "fundamentals.teleport.others")) {
+//                    commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("no_permission"));
+//                    return true;
+//                }
+//                //Get Players
+//                Player player1 = getPlayerFromString(strings[0]);
+//                Player player2 = getPlayerFromString(strings[1]);
+//
+//                if (player1 == null) {
+//                    String message = FundamentalsLanguage.getInstance().getMessage("player_not_found_full");
+//                    message = message.replace("%username%", strings[0]);
+//                    commandSender.sendMessage(message);
+//                    return true;
+//                }
+//                if (player2 == null) {
+//                    String message = FundamentalsLanguage.getInstance().getMessage("player_not_found_full");
+//                    message = message.replace("%username%", strings[1]);
+//                    commandSender.sendMessage(message);
+//                    return true;
+//                }
+//
+//                player1.teleport(player2.getLocation());
+//
+//
+//            case 3: //Teleport to a set of cords
+//            case 4: //Teleport a player to a set of cords
+//
+//        }
+//
+//
+//        if (strings.length > 0) {
+//            if (!isPlayerAuthorized(commandSender, "fundamentals.god.others")) {
+//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("no_permission"));
+//                return true;
+//            }
+//            Player giveTo = getPlayerFromString(strings[0]);
+//            if (giveTo == null) {
+//                String message = FundamentalsLanguage.getInstance().getMessage("player_not_found_full");
+//                message = message.replace("%username%", strings[0]);
+//                commandSender.sendMessage(message);
+//                return true;
+//            }
+//            boolean currentGodmode = FundamentalsPlayerMap.getInstance().getPlayer(giveTo).getFileGodModeStatus();
+//            currentGodmode = !currentGodmode;
+//            FundamentalsPlayerMap.getInstance().getPlayer(giveTo).setFileGodModeStatus(currentGodmode);
+//            if (currentGodmode) {
+//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("god_enable"));
+//            } else {
+//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("god_disable"));
+//            }
+//
+//        } else {
+//            if (!(commandSender instanceof Player)) {
+//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("unavailable_to_console"));
+//                return true;
+//            }
+//            Player giveTo = (Player) commandSender;
+//            boolean currentGodmode = FundamentalsPlayerMap.getInstance().getPlayer(giveTo).getFileGodModeStatus();
+//            currentGodmode = !currentGodmode;
+//            FundamentalsPlayerMap.getInstance().getPlayer(giveTo).setFileGodModeStatus(currentGodmode);
+//            if (currentGodmode) {
+//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("god_enable"));
+//            } else {
+//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("god_disable"));
+//            }
+//        }
+//        return true;
+//
+//
+//    }
---
+++ FundamentalsCore/src/com/johnymuffin/beta/fundamentals/importer/UUIDFetcherWrapper.java
@@ -0,0 +1,130 @@
+package com.johnymuffin.beta.fundamentals.importer;
+
+import com.johnymuffin.beta.fundamentals.Fundamentals;
+import com.johnymuffin.beta.fundamentals.simplejson.JSONObject;
+import com.johnymuffin.beta.fundamentals.simplejson.parser.JSONParser;
+import com.johnymuffin.beta.fundamentals.simplejson.parser.ParseException;
+import com.projectposeidon.api.PoseidonUUID;
+import com.projectposeidon.johnymuffin.UUIDManager;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class UUIDFetcherWrapper {
+    private List<String> knownPlayers;
+    private HashMap<String, UUID> uuidResults = new HashMap<String, UUID>();
+    private Fundamentals plugin;
+    private long lastNotification;
+    private int online = 0;
+    private int offline = 0;
+    private int errored = 0;
+    private int cache = 0;
+
+    public UUIDFetcherWrapper(List<String> knownPlayers, Fundamentals plugin) {
+        lastNotification = 0L;
+        this.knownPlayers = knownPlayers;
+        this.plugin = plugin;
+
+    }
+
+    public HashMap<String, UUID> call() throws Exception {
+        for (String username : this.knownPlayers) {
+            UUID uuid = PoseidonUUID.getPlayerMojangUUID(username);
+            if (uuid != null) {
+                online++;
+                uuidResults.put(username, uuid);
+                continue;
+            }
+            long unixTime = (System.currentTimeMillis() / 1000L);
+            //Check for cracked users
+            uuid = UUIDManager.getInstance().getUUIDFromUsername(username, false, unixTime);
+            if (uuid != null) {
+                offline++;
+                uuidResults.put(username, null);
+                continue;
+            }
+            unixTime = unixTime + 1382400;
+            try {
+                uuid = getUUID(username);
+                if (uuid != null) {
+                    cache++;
+                    UUIDManager.getInstance().receivedUUID(username, uuid, unixTime, true);
+                } else {
+                    offline++;
+                    UUIDManager.getInstance().receivedUUID(username, UUIDManager.generateOfflineUUID(username), unixTime, false);
+                }
+                uuidResults.put(username, uuid);
+            } catch (Exception e) {
+                errored++;
+                plugin.logger(Level.WARNING, "Unable to fetch UUID for " + username + " due to a Mojang error, " + e + " : " + e.getMessage());
+            }
+
+            if (lastNotification + 30 < System.currentTimeMillis() / 1000L) {
+                lastNotification = System.currentTimeMillis() / 1000L;
+                int total = offline + online + errored;
+                plugin.logger(Level.INFO, "UUID Convert Progress: " + total + "/" + this.knownPlayers.size() + ", Cache: " + cache + ", Online: " + online + ", Offline: " + offline + ", Errored: " + errored);
+            }
+
+            Thread.sleep(300);
+        }
+        int total = offline + online + errored + cache;
+        plugin.logger(Level.INFO, "UUID Convert Complete: " + total + "/" + this.knownPlayers.size() + ", Cache: " + cache + ", Online: " + online + ", Offline: " + offline + ", Errored: " + errored);
+        return uuidResults;
+
+    }
+
+
+    private UUID getUUID(String username) throws ParseException, IOException {
+        JSONObject jsonObject = readJsonFromUrl("https://api.minetools.eu/uuid/" + username);
+        if (String.valueOf(jsonObject.get("status")).equalsIgnoreCase("ERR")) {
+            return null;
+        } else {
+            return UUID.fromString(insertDashUUID(String.valueOf(jsonObject.get("id"))));
+        }
+
+
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static String insertDashUUID(String uuid) {
+        StringBuilder sb = new StringBuilder(uuid);
+        sb.insert(8, "-");
+        sb = new StringBuilder(sb.toString());
+        sb.insert(13, "-");
+        sb = new StringBuilder(sb.toString());
+        sb.insert(18, "-");
+        sb = new StringBuilder(sb.toString());
+        sb.insert(23, "-");
+
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, ParseException {
+
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+
+}
