3f9e1f7d31edfa6ee79abd7a1879219f483d1cb4
RhysB <github@johnymuffin.com>
2021-01-06 09:17:18 +1000

    UUIDCache, EconomyCache, God mode fix, Basic Economy Event, Vanish Logic 2

--- FundamentalsCore/src/com/johnymuffin/beta/fundamentals/events/FundamentalsEvent.java
+++ FundamentalsCore/src/com/johnymuffin/beta/fundamentals/events/FundamentalsEvent.java
@@ -1,2 +1,17 @@
-package com.johnymuffin.beta.fundamentals.events;public class FundamentalsEvent {
+package com.johnymuffin.beta.fundamentals.events;
+
+import com.johnymuffin.beta.fundamentals.Fundamentals;
+import org.bukkit.event.Event;
+
+public class FundamentalsEvent extends Event {
+    private final Fundamentals plugin;
+
+    protected FundamentalsEvent(String eventName, Fundamentals plugin) {
+        super(eventName);
+        this.plugin = plugin;
+    }
+
+    public Fundamentals getPlugin() {
+        return plugin;
+    }
 }
--- FundamentalsCore/src/com/johnymuffin/beta/fundamentals/events/FundamentalsPlayerEvent.java
+++ FundamentalsCore/src/com/johnymuffin/beta/fundamentals/events/FundamentalsPlayerEvent.java
@@ -1,2 +1,17 @@
-package com.johnymuffin.beta.fundamentals.events;public class FundamentalsPlayerEvent {
+package com.johnymuffin.beta.fundamentals.events;
+
+import com.johnymuffin.beta.fundamentals.Fundamentals;
+import com.johnymuffin.beta.fundamentals.player.FundamentalsPlayer;
+
+public class FundamentalsPlayerEvent extends FundamentalsEvent{
+    private final FundamentalsPlayer fundamentalsPlayer;
+
+    protected FundamentalsPlayerEvent(String eventName, Fundamentals plugin, FundamentalsPlayer fundamentalsPlayer) {
+        super(eventName, plugin);
+        this.fundamentalsPlayer = fundamentalsPlayer;
+    }
+
+    public FundamentalsPlayer getFundamentalsPlayer() {
+        return fundamentalsPlayer;
+    }
 }
