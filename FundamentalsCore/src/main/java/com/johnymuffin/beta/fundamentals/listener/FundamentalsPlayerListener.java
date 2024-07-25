c6a15582b73c0fecf3313e1f5b0fc36c2478d2b0
zavdav <ruc220228@spengergasse.at>
2024-07-26 00:42:31 +0200

    Removed unnecessary import

--- FundamentalsCore/src/main/java/com/johnymuffin/beta/fundamentals/listener/FundamentalsPlayerListener.java
+++ FundamentalsCore/src/main/java/com/johnymuffin/beta/fundamentals/listener/FundamentalsPlayerListener.java
@@ -20,7 +20,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.*;
 
 import java.util.HashSet;
