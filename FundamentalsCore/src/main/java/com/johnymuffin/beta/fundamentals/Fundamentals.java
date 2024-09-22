38134b7a3f59bb27ab13d7c32bd0d9504badf7ab
zavdav <ruc220228@spengergasse.at>
2024-09-22 21:02:02 +0200

    Register /msg & /r

--- FundamentalsCore/src/main/java/com/johnymuffin/beta/fundamentals/Fundamentals.java
+++ FundamentalsCore/src/main/java/com/johnymuffin/beta/fundamentals/Fundamentals.java
@@ -157,6 +157,8 @@ public class Fundamentals extends JavaPlugin {
 //        Bukkit.getPluginCommand("clearinventory").setExecutor(new CommandClearInventory(plugin));
 //        Bukkit.getPluginCommand("time").setExecutor(new CommandTime(plugin));
 //        Bukkit.getPluginCommand("ptime").setExecutor(new CommandPTime(plugin));
+        Bukkit.getPluginCommand("msg").setExecutor(new CommandMsg(plugin));
+        Bukkit.getPluginCommand("r").setExecutor(new CommandR(plugin));
         Bukkit.getPluginCommand("mute").setExecutor(new CommandMute(plugin));
         Bukkit.getPluginCommand("ignore").setExecutor(new CommandIgnore(plugin));
         Bukkit.getPluginCommand("vanish").setExecutor(new CommandVanish(plugin));
