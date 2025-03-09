9a791c4ad251e7b1e5517b5856bd1804e37c0185
xXGunner989Xx <williamkaye94@gmail.com>
2025-03-08 20:45:07 -0500

    Added /back

--- FundamentalsCore/src/main/java/com/johnymuffin/beta/fundamentals/commands/CommandTeleport.java
+++ FundamentalsCore/src/main/java/com/johnymuffin/beta/fundamentals/commands/CommandTeleport.java
@@ -1,104 +1,107 @@
 //package com.johnymuffin.beta.fundamentals.commands;
 //
-//import com.johnymuffin.beta.fundamentals.FundamentalsPlayerMap;
-//import com.johnymuffin.beta.fundamentals.settings.FundamentalsLanguage;
+//import com.johnymuffin.beta.fundamentals.Fundamentals;
+//import org.bukkit.Bukkit;
+//import org.bukkit.Location;
 //import org.bukkit.command.Command;
 //import org.bukkit.command.CommandExecutor;
 //import org.bukkit.command.CommandSender;
 //import org.bukkit.entity.Player;
 //
-//import static com.johnymuffin.beta.fundamentals.util.CommandUtils.getPlayerFromString;
-//import static com.johnymuffin.beta.fundamentals.FundamentalPermission.isPlayerAuthorized;
-//
 //public class CommandTeleport implements CommandExecutor {
-//    @Override
-//    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-//        if (!isPlayerAuthorized(commandSender, "fundamentals.teleport")) {
-//            commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("no_permission"));
+//
+//    private Fundamentals plugin;
+//
+//    public CommandTeleport(Fundamentals plugin) {
+//        this.plugin = plugin;
+//    }
+//
+//    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+//        // Check if sender has teleport permission (customize as needed)
+//        if (!sender.hasPermission("fundamentals.teleport")) {
+//            sender.sendMessage("You do not have permission to use this command.");
 //            return true;
 //        }
 //
+//        try {
+//            switch (args.length) {
+//                case 1: // /tp <targetPlayerName>
+//                    return teleportToPlayer(sender, args[0]);
 //
-//        switch (strings.length) {
-//            case 0:
-//                //Not Enough Args
-//            case 1: //Teleporting to a player
-//                Player teleportTo = getPlayerFromString(strings[0]);
-//                if (teleportTo == null) {
-//                    String message = FundamentalsLanguage.getInstance().getMessage("player_not_found_full");
-//                    message = message.replace("%username%", strings[0]);
-//                    commandSender.sendMessage(message);
+//                case 3: // /tp <x> <y> <z>
+//                    return teleportToCoordinates(sender, args);
+//
+//                case 4: // /tp <targetPlayerName> <x> <y> <z>
+//                    return teleportPlayerToCoordinates(sender, args[0], args[1], args[2], args[3]);
+//
+//                case 2: // /tp <targetPlayerName> <destinationPlayerName>
+//                    return teleportPlayerToPlayer(sender, args[0], args[1]);
+//
+//                default:
+//                    sender.sendMessage("Usage: /tp <player> [destinationPlayer] or /tp [x y z] or /tp <player> [x y z]");
 //                    return true;
-//                }
-//                //Teleport Player
-//            case 2: //Teleport one player to another
-//                if (!isPlayerAuthorized(commandSender, "fundamentals.teleport.others")) {
-//                    commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("no_permission"));
-//                    return true;
-//                }
-//                //Get Players
-//                Player player1 = getPlayerFromString(strings[0]);
-//                Player player2 = getPlayerFromString(strings[1]);
-//
-//                if (player1 == null) {
-//                    String message = FundamentalsLanguage.getInstance().getMessage("player_not_found_full");
-//                    message = message.replace("%username%", strings[0]);
-//                    commandSender.sendMessage(message);
-//                    return true;
-//                }
-//                if (player2 == null) {
-//                    String message = FundamentalsLanguage.getInstance().getMessage("player_not_found_full");
-//                    message = message.replace("%username%", strings[1]);
-//                    commandSender.sendMessage(message);
-//                    return true;
-//                }
-//
-//                player1.teleport(player2.getLocation());
-//
-//
-//            case 3: //Teleport to a set of cords
-//            case 4: //Teleport a player to a set of cords
-//
+//            }
+//        } catch (Exception e) {
+//            sender.sendMessage("An error occurred while executing the command.");
+//            return true;
 //        }
-//
-//
-//        if (strings.length > 0) {
-//            if (!isPlayerAuthorized(commandSender, "fundamentals.god.others")) {
-//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("no_permission"));
-//                return true;
-//            }
-//            Player giveTo = getPlayerFromString(strings[0]);
-//            if (giveTo == null) {
-//                String message = FundamentalsLanguage.getInstance().getMessage("player_not_found_full");
-//                message = message.replace("%username%", strings[0]);
-//                commandSender.sendMessage(message);
-//                return true;
-//            }
-//            boolean currentGodmode = FundamentalsPlayerMap.getInstance().getPlayer(giveTo).getFileGodModeStatus();
-//            currentGodmode = !currentGodmode;
-//            FundamentalsPlayerMap.getInstance().getPlayer(giveTo).setFileGodModeStatus(currentGodmode);
-//            if (currentGodmode) {
-//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("god_enable"));
-//            } else {
-//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("god_disable"));
-//            }
-//
-//        } else {
-//            if (!(commandSender instanceof Player)) {
-//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("unavailable_to_console"));
-//                return true;
-//            }
-//            Player giveTo = (Player) commandSender;
-//            boolean currentGodmode = FundamentalsPlayerMap.getInstance().getPlayer(giveTo).getFileGodModeStatus();
-//            currentGodmode = !currentGodmode;
-//            FundamentalsPlayerMap.getInstance().getPlayer(giveTo).setFileGodModeStatus(currentGodmode);
-//            if (currentGodmode) {
-//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("god_enable"));
-//            } else {
-//                commandSender.sendMessage(FundamentalsLanguage.getInstance().getMessage("god_disable"));
-//            }
-//        }
-//        return true;
-//
-//
 //    }
+//
+//    private boolean teleportToCoordinates(CommandSender sender, String[] args) {
+//        if (!(sender instanceof Player)) {
+//            sender.sendMessage("Only players can use this command.");
+//            return true;
+//        }
+//
+//        Player player = (Player) sender;
+//        Location loc = new Location(player.getWorld(), Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]));
+//        player.teleport(loc);
+//        sender.sendMessage("Teleported to coordinates.");
+//        return true;
+//    }
+//
+//    private boolean teleportPlayerToCoordinates(CommandSender sender, String playerName, String x, String y, String z) {
+//        Player player = Bukkit.getPlayer(playerName);
+//        if (player == null) {
+//            Bukkit.getServer().getConsoleSender().sendMessage("Player not found.");
+//
+//            return true;
+//        }
+//
+//        Location loc = new Location(player.getWorld(), Double.parseDouble(x), Double.parseDouble(y), Double.parseDouble(z));
+//        player.teleport(loc);
+//        Bukkit.getServer().getConsoleSender().sendMessage("Teleported " + playerName + " to coordinates.");
+//        return true;
+//    }
+//
+//    private boolean teleportToPlayer(CommandSender sender, String targetPlayerName) {
+//        if (!(sender instanceof Player)) {
+//            sender.sendMessage("Only players can use this command.");
+//            return true;
+//        }
+//
+//        Player player = (Player) sender;
+//        Player targetPlayer = Bukkit.getPlayer(targetPlayerName);
+//        if (targetPlayer == null) {
+//            sender.sendMessage("Target player not found.");
+//            return true;
+//        }
+//
+//        player.teleport(targetPlayer.getLocation());
+//        sender.sendMessage("Teleported to " + targetPlayerName + ".");
+//        return true;
+//    }
+//
+//    private boolean teleportPlayerToPlayer(CommandSender sender, String playerName, String destinationPlayerName) {
+//        Player player = Bukkit.getPlayer(playerName);
+//        Player destinationPlayer = Bukkit.getPlayer(destinationPlayerName);
+//        if (player == null || destinationPlayer == null) {
+//            Bukkit.getServer().getConsoleSender().sendMessage("Player not found.");
+//            return true;
+//        }
+//
+//        player.teleport(destinationPlayer.getLocation());
+//        Bukkit.getServer().getConsoleSender().sendMessage("Teleported " + playerName + " to " + destinationPlayerName + ".");
+//        return true;
+//    }
+//}
